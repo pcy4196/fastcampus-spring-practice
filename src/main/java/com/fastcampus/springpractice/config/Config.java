8517a55fb6e3b8d5f63d8155f718d52d634a0fd9
Park, Chan-Yu <pcy4196@naver.com>
2022-04-09 01:43:11 +0900

    change #1 - package 복사를 그대로 하여 패키지 변경 add #1 - config config bean등록 -> component scan 가능하게 설정

---
+++ src/main/java/com/fastcampus/springpractice/config/Config.java
@@ -0,0 +1,11 @@
+package com.fastcampus.springpractice.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+
+@ComponentScan("com.fastcampus.springpractice")
+@Configuration
+public class Config {
+    // 스프링 전용 config class
+}
--- src/test/java/com/fastcampus/springpractice/logic/BubbleSortTest.java
+++ src/test/java/com/fastcampus/springpractice/logic/BubbleSortTest.java
@@ -1,4 +1,4 @@
-package com.fastcampus.javaoop.logic;
+package com.fastcampus.springpractice.logic;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -7,16 +7,16 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class JavaSortTest {
+class BubbleSortTest {
 
-    @DisplayName("자바소트 - 리스트를 넣으면 정렬된 결과를 보여준다.")
+    @DisplayName("버블소트 - 리스트를 넣으면 정렬된 결과를 보여준다.")
     @Test
     void givenListWhenExecutingThenReturnSortedList() {
         // Given
-        BubbleSort<Integer> javaSort = new BubbleSort<>();
+        BubbleSort<Integer> bubbleSort = new BubbleSort<>();
 
         // When
-        List<Integer> actual = javaSort.sort(List.of(3,2,4,5,1));
+        List<Integer> actual = bubbleSort.sort(List.of(3,2,4,5,1));
 
         // then
         assertEquals(List.of(1,2,3,4,5), actual);
